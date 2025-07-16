ed70793c7599b466f1f4d2d54956ee983cd77c6e
Rohim Maruf <rohim.maruf26@gmail.com>
2025-07-16 22:18:28 +0700

    Halaman utama

--- app/ticket/home.jsx
+++ app/ticket/home.jsx
@@ -1,5 +1,39 @@
 import { Image, StyleSheet, Text, View } from "react-native";
 
+const listMenu = [
+    {
+        id: 1,
+        img : require('../../assets/images/splash.png'),
+        label : 'Bioskop'
+    },
+    {
+        id: 2,
+        img : require('../../assets/images/kereta.png'),
+        label : 'Travel'
+    },
+    {
+        id: 3,
+        img : require('../../assets/images/plane.png'),
+        label : 'Pesawat'
+    },
+    {
+        id: 4,
+        img : require('../../assets/images/hotel.png'),
+        label : 'Hotel'
+    },
+    {
+        id: 5,
+        img : require('../../assets/images/wisata.png'),
+        label : 'Wisata'
+    },
+    {
+        id: 6,
+        img : require('../../assets/images/spot.png'),
+        label : 'Spot'
+    },
+    
+]
+
 const Home = () => {
     const {textContain2, contain3} = style
     return (
@@ -61,21 +95,53 @@ const Home = () => {
                 />
             </View>
             {/* Contain 3 */}
-            <View style={contain3}>
-            <View >
+            <View style={{ alignItems: 'center'}}>
+                <View style={contain3}>
+                {/* Contain 3.1 */}
+            <View style={
+                {
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    gap: 10,
+                    marginHorizontal: 40,
+                    
+
+                }
+                }>
                 <Image
                     source={require('../../assets/images/Wallet.png')}
                     style={{
                         color: '#FFFFFF',
+                        width: 40,
+                        height: 40
                     }}
                 />
                 <Text style={{color: '#7B22D3', fontWeight: 'bold'}}>Rp 5.000.000</Text>
+                {/* contain 3.2 */}
             </View>
-            <View style={{flexDirection: 'row', alignItems:'center'}}>
+            <View style={{
+                flexDirection: 'row', 
+                alignItems:'center',
+                marginHorizontal: 40,
+                gap: 10
+                
+                }}>
                 <Image
                     source={require('../../assets/images/scan.png')}
+                    style={{
+                        width: 40,
+                        height: 40
+                    }}
                 />
-                <Text>SCAN</Text>
+                <Text style={{color:'#7B22D3', fontWeight: 'bold'}}>SCAN</Text>
+            </View>
+            </View>
+            {/* Contain 4 */}
+            <View style={{}}>
+                <Image
+                    source={require('../../assets/images/hotel.png')}
+                />
+
             </View>
             </View>
 
@@ -95,7 +161,11 @@ const style = StyleSheet.create(
         },
         contain3 : {
             backgroundColor: '#FFFFFF',
-            flexDirection: 'row'
+            flexDirection: 'row',
+            width:400,
+            height: 90,
+            borderRadius: 10,
+            justifyContent: 'space-between'
         }
     }
 )
